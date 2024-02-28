017e2a804c45a3d6b924f4e4dc4694b0d37dcf1f
Anas <abidi.anas.jshb999@gmail.com>
2024-02-28 15:15:53 +0100

    v1 login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigationbar from './components/Navigationbar'
+import Home from './components/Home'
+import Medecin from './components/Medecin'
 function App() {
   return (
+  <>
+
+<BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navigationbar/>
+   
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        
+        <Route path='/medecin' element={<Medecin/>} />
+        <Route path='*' element={<h1>Page Not Found</h1>} />
+        
+      </Routes>
     </div>
+    </BrowserRouter>
+
+
+  </>
   );
 }
 
